a80f918b95edccbf19ec404806004de53594a65a
Lucas Caccavaro <andradecaccavarolucas@johndeere.com>
2020-11-04 18:22:08 -0300

    Implemented styles and image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
+
 import Header from './components/Header';
 
+import './App.css';
+
+import background from './assets/background.jpg';
+
 function App() {
+
+    const [projects, setProjects] = useState(['Project 1', 'Project 2']);
+    const [projectIndex, setProjectIndex] = useState(3);
+
+    function handleAddProject() {
+        //Imutability concept: you cannot update the variable, you must create again.
+        setProjects([...projects, `Project ${projectIndex}`]);
+        setProjectIndex(projectIndex => projectIndex + 1);
+    }
+
     return (
         <>
-            <Header title='Home'>
-                <ul>
-                    <li>Home</li>
-                    <li>Profile</li>
-                    <li>Logout</li>
-                </ul>
-            </Header>
-            <Header title='Projects'>
-                <ul>
-                    <li>New</li>
-                    <li>Upload</li>
-                </ul>
-            </Header>
+            <Header title='Projects' />
+            <img src={background} width={300}/>
+            <ul>
+                {projects.map(project => (
+                    <li key={project}>{project}</li>
+                ))}
+            </ul>
+            <button onClick={handleAddProject}>Add project</button>
         </>
     )
 }
